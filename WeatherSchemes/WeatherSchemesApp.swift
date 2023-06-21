30043ef18037adcdbb16ac583e8a0223ce9e7155
phillipchan1 <phillipchan1@gmail.com>
2023-06-21 07:27:44 -0700

    Initial Commit

---
+++ WeatherSchemes/WeatherSchemesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSchemesApp.swift
+//  WeatherSchemes
+//
+//  Created by Phil Chan on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSchemesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
