0338f68711cfb7adfec5f67359f85e046cd7e46e
phillipchan1 <phillipchan1@gmail.com>
2023-06-21 07:41:55 -0700

    first commit

---
+++ WeatherSubApp/WeatherSubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSubAppApp.swift
+//  WeatherSubApp
+//
+//  Created by Phil Chan on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
